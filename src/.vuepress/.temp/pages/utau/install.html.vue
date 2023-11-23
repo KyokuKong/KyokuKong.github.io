2e7c4981adb966b6497d34fae465fb3e4affd4bf
Kyoku急急急极 <feikongtl@gmail.com>
2023-11-24 04:11:00 +0800

    23.11.24b

--- src/.vuepress/.temp/pages/utau/install.html.vue
+++ src/.vuepress/.temp/pages/utau/install.html.vue
@@ -8,6 +8,20 @@ MacOS我也想讲，奈何买不起Mac（泪目）</p>
 <p>截止本篇文档最后一次被编辑时的OpenUtau正式发行版本为 <a href="https://github.com/stakira/OpenUtau/releases/tag/build%2F0.1.158" target="_blank" rel="noopener noreferrer">V0.1.158<ExternalLinkIcon/></a> Release。尽管不同版本的操作一般并不会有什么太大的区别，但我仍然建议你优先根据实际情况进行操作。</p>
 <p>如果一直没有跟进版本那么欢迎评论区催更~</p>
 </div>
+<h2 id="下载及安装" tabindex="-1"><a class="header-anchor" href="#下载及安装" aria-hidden="true">#</a> 下载及安装</h2>
+<p>OpenUtau是一款免费开源的共享软件，你可以直接在<a href="http://www.openutau.com/" target="_blank" rel="noopener noreferrer">官网<ExternalLinkIcon/></a>或者<a href="https://github.com/stakira/OpenUtau/releases" target="_blank" rel="noopener noreferrer">Github Release<ExternalLinkIcon/></a>下载到最新的版本。</p>
+<figure><img src="@source/.vuepress/public/download_openutau.png" alt="直接选择对应自己操作系统环境的下载链接即可" tabindex="0" loading="lazy"><figcaption>直接选择对应自己操作系统环境的下载链接即可</figcaption></figure>
+<div class="hint-container tip">
+<p class="hint-container-title">提示</p>
+<p>一般来讲，你应该选择<code v-pre>*Windows X64*</code>对应的下载项。</p>
+<p>很显然，2023年了，你的电子产品一般来讲都是64位的。</p>
+</div>
+<div class="hint-container note">
+<p class="hint-container-title">注</p>
+<p>由于一些一言难尽的原因，直接通过官网或者release下载安装程序有时候速度会相当悲惨。</p>
+<p>如果你在尝试过使用多线程下载器等方式后仍然不能正常的进行下载，我也提供了<strong>云盘版本</strong>。</p>
+<p>云盘版本可能会无法马上跟进每次更新，当前云盘版本为0.1.158。</p>
+</div>
 </div></template>
 
 
